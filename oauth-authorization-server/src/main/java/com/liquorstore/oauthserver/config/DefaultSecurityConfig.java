11952ecddca74724b0c3e9b2b570543fe6b3ccaf
mittulmandhan <conanmittul@gmail.com>
2022-11-18 16:45:22 +0530

    client authorization configuration completed

--- oauth-authorization-server/src/main/java/com/liquorstore/oauthserver/config/DefaultSecurityConfig.java
+++ oauth-authorization-server/src/main/java/com/liquorstore/oauthserver/config/DefaultSecurityConfig.java
@@ -1,2 +1,32 @@
-package com.liquorstore.oauthserver.config;public class DefaultSecurityConfig {
+package com.liquorstore.oauthserver.config;
+
+import com.liquorstore.oauthserver.service.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(
+                authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+        ).formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    public  void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    authenticationManagerBuilder
+            .authenticationProvider(customAuthenticationProvider);
+    }
+
 }
